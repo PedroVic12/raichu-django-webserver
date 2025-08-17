5361793524456f4b2ede8d2e9881e27c6c606fdf
Pedro Victor Veras <pedrovictor.rveras12@gmail.com>
2025-08-17 05:52:39 -0300

    up

--- backend/core/views.py
+++ backend/core/views.py
@@ -4,3 +4,11 @@ from django.shortcuts import render
 def index(request):
     template_name = 'core/index.html'
     return render(request, template_name)
+
+def expense_list(request):
+    template_name = 'expense/expense.html'
+    form = ExpenseForm()
+    context = {'form': form}
+    return render(request, template_name, context)
+
+
